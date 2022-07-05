0b33058c35dd027aa262ebe7e130fd84f2755a7c
mehmet <tas349873@gmail.com>
2022-07-05 22:36:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import {ThemeProvider} from 'styled-components';
+
+
+const theme ={
+  colors:{
+    Header:'#886',
+    body: '#fff',
+    footer: '#8A1C4A'
+  },
+  responsive:"768px",
+};
+
+
+
+
+
+const App = () => {
+  return (
+  <ThemeProvider theme={theme}>
+    <Header/>
+  </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import Button from "./styles/Button.styled";
+import Container from "./styles/Container.styled";
+import StyledHeader, { Logo, Nav } from "./styles/Header.style";
+
+const Header = () => {
+    return (
+        <StyledHeader>
+            <Container>
+            <Nav>
+                <Logo src="./images/logo.png"></Logo>
+                <div>
+                    <Button color="#A62440">Apply Courses</Button>
+                    <Button bg="#A62440">Talk to Advisor</Button>
+                </div>
+            </Nav>
+            <div>
+          <h1>The IT Career of Your Dreams Starts Here!</h1>
+          <p>
+          
+            Clarusway is a leading international software Bootcamp. Join a micro
+            class online with other trainees and learn coding skills with a
+            highly-skilled instructor.
+          </p>
+        </div>
+        </Container>
+        </StyledHeader>
+    );
+};
+
+export default Header;
