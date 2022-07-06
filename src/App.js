829dce8957585b5111961b337eab8bdec9aef6cc
mehmet <tas349873@gmail.com>
2022-07-06 14:10:33 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import Header from "./components/Header";
-import {ThemeProvider} from 'styled-components';
-import { GlobalStlyes } from "./components/styles/global";
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStlyes } from './components/styles/Global';
+import data from './data';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import Container from './components/styles/Container.styled';
 
-
-const theme ={
-  colors:{
-    Header:'#886',
+const theme = {
+  colors: {
+    header: '#fff',
     body: '#fff',
-    footer: '#8A1C4A'
+    footer: '#8A1C4A',
   },
-  responsive:"768px",
+  responsive: '768px',
 };
-
-
-
-
-
 const App = () => {
   return (
-  <ThemeProvider theme={theme}>
-    <GlobalStlyes/>
-    <Header/>
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStlyes />
+      <Header />
+      <Container>
+        {data.map((item, index) => {
+          return <Card {...item} key={index} />;
+        })}
+      </Container>
+      <Footer />
+    </ThemeProvider>
   );
 };
 
